fe657d390bb8bc89265d240ed4ca69dbaff6b8de
Avantika Gupta <avantikagupta1729@gmail.com>
2023-05-07 19:31:37 +0530

    Third Commit

--- Python Object and Data Structure Basics/Dictionaries.py
+++ Python Object and Data Structure Basics/Dictionaries.py
@@ -5,6 +5,7 @@ print(priceLookup["apple"])
 d = {'k1': 123, 'k2': [0, 1, 2], 'k3': {'insideKey': "helloworld"}}
 print(d['k1'])
 print(d['k2'])
+print(d['k2'][2])
 print(d['k3'])
 print(d['k3']['insideKey'])
 print(d['k3']['insideKey'].upper())
@@ -13,3 +14,12 @@ print(d.keys())
 # printing the values
 print(d.values())
 print(d.items())
+# Very Important
+d = {'simplekey': "Hello"}
+print(d['simplekey'])
+d = {'k1': {'k2': "Hello"}}
+print(d['k1']['k2'])
+d = {'k1': [{'nestkey': ['this is deep', ['Hello']]}]}
+print(d['k1'][0]['nestkey'][1][0])
+d = {'k1': [1, 2, {'k2': ['this is tricky', {'tough': [1, 2, ['Hello']]}]}]}
+print(d['k1'][2]['k2'][1]['tough'][2][0])
